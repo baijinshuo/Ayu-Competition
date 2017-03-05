8dd3bb69046877d3b7196954f0936db3d95e7e30
Jinshuo Bai <baijinshuo_1209@hotmail.com>
2017-03-04 23:01:52 -0500

    Create LoginFilter.java

---
+++ ayu-wechat/src/com/template/commons/filter/LoginFilter.java
@@ -0,0 +1,30 @@
+package com.template.commons.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class LoginFilter implements Filter {
+
+	@Override
+	public void destroy() {
+		
+	}
+
+	@Override
+	public void doFilter(ServletRequest arg0, ServletResponse arg1, FilterChain arg2)
+			throws IOException, ServletException {
+		
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+		
+	}
+
+}
