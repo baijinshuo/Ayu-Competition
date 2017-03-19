930d2474a03035b11dc7cffe9af4f32dceed6920
Jinshuo Bai <baijinshuo_1209@hotmail.com>
2017-03-19 10:12:38 -0400

    Create Guess.java

---
+++ ayu-wechat/src/com/template/framework/domain/Guess.java
@@ -0,0 +1,65 @@
+package com.template.framework.domain;
+
+import java.util.Date;
+
+public class Guess {
+    private Integer id;
+
+    private String issue;
+
+    private Date startTime;
+
+    private Date endTime;
+
+    private Integer status;
+
+    private Date createTime;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getIssue() {
+        return issue;
+    }
+
+    public void setIssue(String issue) {
+        this.issue = issue;
+    }
+
+    public Date getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(Date startTime) {
+        this.startTime = startTime;
+    }
+
+    public Date getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(Date endTime) {
+        this.endTime = endTime;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+}
