85dbd3b74f2ab8c15f0560c18934d980284a7b55
Jinshuo Bai <baijinshuo_1209@hotmail.com>
2017-03-22 10:55:48 -0400

    Create Manage.java

---
+++ ayu-wechat/src/com/template/framework/domain/Manage.java
@@ -0,0 +1,65 @@
+package com.template.framework.domain;
+
+import java.util.Date;
+
+public class Manage {
+    private Integer id;
+
+    private String matchName;
+
+    private Integer matchType;
+
+    private Integer status;
+
+    private Date startTime;
+
+    private Date endTime;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getMatchName() {
+        return matchName;
+    }
+
+    public void setMatchName(String matchName) {
+        this.matchName = matchName;
+    }
+
+    public Integer getMatchType() {
+        return matchType;
+    }
+
+    public void setMatchType(Integer matchType) {
+        this.matchType = matchType;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public Date getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(Date startTime) {
+        this.startTime = startTime;
+    }
+
+    public Date getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(Date endTime) {
+        this.endTime = endTime;
+    }
+}
