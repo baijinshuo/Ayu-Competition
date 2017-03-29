42bc02f49d6218410f27e166ab9f8134ca2f7297
Jinshuo Bai <baijinshuo_1209@hotmail.com>
2017-03-28 23:57:20 -0400

    Create GuessItemList.java

---
+++ ayu-wechat/src/com/template/framework/vo/GuessItemList.java
@@ -0,0 +1,17 @@
+package com.template.framework.vo;
+
+import java.util.List;
+
+import com.template.framework.domain.GuessItem;
+
+public class GuessItemList {
+	private List<GuessItem> items;
+
+	public List<GuessItem> getItems() {
+		return items;
+	}
+
+	public void setItems(List<GuessItem> items) {
+		this.items = items;
+	}
+}
